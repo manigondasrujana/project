db7d9543f0ddec1687527375f647669e91816228
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-10-20 12:52:12 -0400

    one

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+SELECT FILE: <br>
+<input type="file" name="fileToUpload" id="fileToUpload"><br>
+<input type="submit" value="upload File" name="submit">
+</form>
+</body>
+</html>
+
+
+
