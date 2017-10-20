4ca5da7c342b59ba085e69d36abcf897332262be
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-10-20 12:55:48 -0400

    three

---
+++ index1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload2.php" method="post" enctype="multipart/form-data">
+      BROWSE THE FILE TO UPLOAD: <br><br>
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <br><br><input type="submit" value="Upload File" name="submit">
+      </form>
+
+      </body>
+      </html>
+
+      
