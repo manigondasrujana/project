f90f367ec9738539e5d0a861dc31606c40243eeb
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-10-22 02:49:16 -0400

    class uploadform

--- index2.php
+++ index2.php
@@ -16,7 +16,7 @@
 	error_reporting(E_ALL);
 	
 	//CLASS INSTANTIATION
-	class Autoload{
+class Autoload{
 	//ATTEMPTS TO LOAD THE CLASS AUTOLOAD
 	public static function autoload($class) {
 	//ENABLING CLASSES TO LOAD AUTOMATICALLY
@@ -29,7 +29,7 @@
 	
 	}
 
-	class main{
+class main{
 	function __construct{
 	//DEFAULT PAGE
 	$requestPage = 'uploadform';
@@ -42,15 +42,63 @@
 	$page = new uploadform();
 
 	if($_SERVER['REQUEST_METHOD'] == 'GET') {
-	//CALLING METHODS OF THE $PAGE ACCEPT $ DISPLAY
-	$page->accept();
+	//CALLING METHODS OF THE $PAGE RECEIVE AND SHOW
+	$page->receive
 	} else {
-	$page->display();
+	$page->show();
 	}
-
+abstract class page {
+    	protected $html;
+        public function __construct()
+	    {
+            $this->html .= '<html>';
+            $this->html .= '<link rel="stylesheet" href="html.css">';
+	    $this->html .= '<body>';
+	    }
+	public function __destruct()
+	    {
+	    $this->html .= '</body></html>';
+	    echo $this->html;
+	    }
+	public function receive()
+	{
+	echo "this is function RECEIVE";
 	}
-
-
+	public function show()
+	{
+	print_r($_POST);
+	}
+	}
+	//INHERITS THE METHODS OF 'PAGE' TO 'UPLOADFORM'
+	class uploadform extends page
+	{
+	//
+	public function receive()
+	{
+	 $form = "<form action='index.php?page=uploadform' method='post' enctype='multipart/form-data'>";
+	 $this->html.=$form;
+	}
+	//
+	function show()
+	{
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+	//
+	if (file_exists($target_file)) {
+	echo "Sorry, file already exists.";
+	}
+	else {
+	//check file type
+	if ($imageFileType != 'csv') {
+	echo 'You can only upload a CSV file';
+	} else {											                   	   //upload file
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+        header("Location:index.php?page=htmlTable&file=$target_file");
+	}
+	}
+	}
+	}
+	}
 	
-?>
-
