3c04792311fccb587d1dd5ad7c534a4a8cd95934
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-10-21 23:42:02 -0400

    php stated

--- index2.php
+++ index2.php
@@ -1,14 +1,20 @@
 <html>
 <head>
   <link rel="stylesheet" href="styles.css">
-  </head>
-  <body>
-
-  <h1>This is PROJECT1</h1>
-  <p>CHOOSE CSV FILE TO UPLOAD</p>
-<input type="file" name="fileToUpload" id="fileToUpload">
-<input type="submit" value="Upload File" name="submit"">
-
+</head>
+<body>
+	<form action="" method="post" enctype="multipart/form-data">
+	<h1>This is PROJECT1</h1>
+	<input type="file" name="fileToUpload" id="fileToUpload">
+	<input type="submit" value="Upload File" name="submit"">
+	<p>CHOOSE CSV FILE TO UPLOAD</p>
 </body>
 </html>
+<?php
+	//DISPLAY ERRORS
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+
+?>
 
